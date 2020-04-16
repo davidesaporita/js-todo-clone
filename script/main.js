cac4a0a205abc30e0dc7ba8503b10bf76caa0a36
Davide Saporita <davide.saporita@gmail.com>
2020-04-16 15:29:10 +0200

    Added project files

---
+++ script/main.js
@@ -0,0 +1,10 @@
+/**
+ * 
+ * ToDo list 
+ * 
+ * Ricreare la todo list vista oggi durante la lezione (focus dell’esercizio la logica)
+ * Usiamo i template in html e il clone per separare la parte logica (JS) dalla parte di markup (HTML)
+ * recap funzionalità: mostrare, aggiungere e eliminare elementi dalla lista
+ * 
+ */
+
